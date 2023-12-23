2e2081d2a8cca2cfdb2d2027dca93c414766a541
rsgur <rafaelgs23@educastur.es>
2023-12-23 15:31:14 +0100

    Finalizado NavigationComponent

--- app/src/main/java/com/mpd/pmdm/navigationcomponent_distancia/ResultFragment.kt
+++ app/src/main/java/com/mpd/pmdm/navigationcomponent_distancia/ResultFragment.kt
@@ -5,55 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.mpd.pmdm.navigationcomponent_distancia.databinding.FragmentResultBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+const val PERRO = 1
+const val GATO = 2
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ResultFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentResultBinding?=null
+    private val binding get() = _binding!!
+    private val args: ResultFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        _binding = FragmentResultBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ResultFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ResultFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvResultName.text = getString(R.string.result_name,args.nombre)
+        binding.tvResultSex.text = getString(R.string.result_sex,args.sexo)
+
+        binding.tvResultPet.text = if(args.mascota == PERRO){ getString(R.string.result_pet,"Perro")
+        }else getString(R.string.result_pet,"Gato")
+
+        binding.btnResultVolver.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+        binding.btnResultTerminar.setOnClickListener {
+            activity?.finish()
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
